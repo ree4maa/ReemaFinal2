6b2e6e5a48ce6a51eec66f0f3051fc37677ca50d
ree4maa <hsreema1107@gmail.com>
2025-11-12 17:35:41 +0200

    bnbn

--- app/src/main/java/com/example/reemafinal2/SplashScreen.java
+++ app/src/main/java/com/example/reemafinal2/SplashScreen.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +17,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        View imageview6 = findViewById(R.id.imageView6);
+        imageview6.animate().alpha(1.0f).setDuration(2000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish(); // حتى لا يعود المستخدم للسبلاش بالرجوع
+            }
+        }, 3000); // 3 ثوانٍ
 
-        VideoView videoView = findViewById(R.id.videoView);
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.intro;
-        Uri uri = Uri.parse(videoPath);
-        videoView.setVideoURI(uri);
 
-        videoView.setOnCompletionListener(mp -> {
-            startActivity(new Intent(SplashScreen.this, Login.class));
-            finish();
-        });
-
-        videoView.start();
     }
 }
