5050aa5e309756c36b3a1447451f9747a8545248
ree4maa <hsreema1107@gmail.com>
2025-11-29 17:17:48 +0200

    bnbn

--- app/src/main/java/com/example/reemafinal2/data/MyTasksTable/MyTaskQuery.java
+++ app/src/main/java/com/example/reemafinal2/data/MyTasksTable/MyTaskQuery.java
@@ -10,6 +10,25 @@ import java.util.List;
 
 @Dao
 public interface MyTaskQuery {
-    @Query("SELECT * FROM MyTask order by imp DESC")
-    List<MyTask> getAll();
+    @Query("SELECT * FROM MyTask order by rewardpoints DESC")
+    List<MyTask> getAllTasks();
+    @Query("SELECT * FROM MyTask WHERE userId = :userId")
+    List<MyTask> getTasksByUserId(String userId);
+    @Query("SELECT * FROM MyTask WHERE subjId = :subjId")
+    List<MyTask> getTasksBySubjId(String subjId);
+    @Query("SELECT * FROM MyTask WHERE gameId = :gameId")
+    List<MyTask> getTasksByGameId(String gameId);
+    @Query("SELECT * FROM MyTask WHERE noteId = :noteId")
+    List<MyTask> getTasksByNoteId(String noteId);
+    @Query("SELECT * FROM MyTask WHERE catagory = :catagory")
+    List<MyTask> getTasksByCatagory(String catagory);
+    @Query("SELECT * FROM MyTask WHERE isCompleted = :isCompleted")
+    List<MyTask> getTasksByIsCompleted(boolean isCompleted);
+    @Query("SELECT * FROM MyTask WHERE time = :time")
+    List<MyTask> getTasksByTime(String time);
+    @Query("SELECT * FROM MyTask WHERE description = :description")
+    List<MyTask> getTasksByDescription(String description);
+    @Query("SELECT * FROM MyTask WHERE title = :title")
+    List<MyTask> getTasksByTitle(String title);
 }
+
