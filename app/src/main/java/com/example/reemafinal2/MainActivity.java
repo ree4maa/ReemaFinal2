53cc2719ed67344063141f3e09f6a05a536844bf
ree4maa <hsreema1107@gmail.com>
2026-02-04 09:41:09 +0200

    bnbn

--- app/src/main/java/com/example/reemafinal2/MainActivity.java
+++ app/src/main/java/com/example/reemafinal2/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.reemafinal2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.example.reemafinal2.data.AppDatabase;
+import com.example.reemafinal2.data.MyTasksTable.MyQuest;
+import com.example.reemafinal2.data.MyTasksTable.MyQuestAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private Spinner spnQuests;
+    private FloatingActionButton fabAddQuest;
+    private ListView lstQuests;
+    private MyQuestAdapter QuestAdapter;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lstQuests = findViewById(R.id.lstQuest);
+        QuestAdapter = new MyQuestAdapter(this, R.layout.quest_item_layout);
+        lstQuests.setAdapter(QuestAdapter);
+
 
            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
 
@@ -46,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        protected void onResume() {
+            super.onResume();
+            List<MyQuest> allQuests = AppDatabase.getDp(this).getMyQuestQuery().getAllQuests();
+            QuestAdapter.clear();
+            QuestAdapter.addAll(allQuests);
+            QuestAdapter.notifyDataSetChanged();
+        }
     }
     }
 
--- app/src/main/java/com/example/reemafinal2/data/MyTasksTable/MyQuestAdapter.java
+++ app/src/main/java/com/example/reemafinal2/data/MyTasksTable/MyQuestAdapter.java
@@ -1,5 +1,52 @@
 package com.example.reemafinal2.data.MyTasksTable;
 
-public class MyQuestAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyQuestAdapter extends ArrayAdapter<MyQuest> {
+    private final int itemLayout;
+
+    public MyQuestAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+        this.itemLayout = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View vitem = convertView;
+        if (vitem == null) {
+            vitem = LayoutInflater.from(getContext()).inflate(itemLayout, parent, false);
+       ImageView imageview = vitem.findViewById(R.id.imgvitm);
+            TextView tvtitle = vitem.findViewById(R.id.TV_title);
+            TextView tvtime = vitem.findViewById(R.id.TV_time);
+            TextView tvgameid = vitem.findViewById(R.id.TV_gameID);
+            TextView tvRewardpoints = vitem.findViewById(R.id.TV_rewardp);
+            ImageButton btnstart = vitem.findViewById(R.id.btn_start);
+            ImageButton btnretry = vitem.findViewById(R.id.btn_retry);
+            ImageButton btnstop = vitem.findViewById(R.id.btn_stop);
+            MyQuest current=getItem(position);
+            tvtitle.setText(current.getTitle());
+            tvtime.setText(current.getTime());
+            tvgameid.setText(current.getGameId());
+            tvRewardpoints.setText(current.getRewardpoints());
+            return vitem;
+
+        }
+    }
 }
+
+
+
+
+
+
